9303360fe9bf94d7ee6cf5d741960165e94f8289
dangkhoa2703 <dangkhoa27031997@gmail.com>
2024-07-17 15:18:29 +0200

    create attack functions for player and monster

---
+++ app.js
@@ -0,0 +1,23 @@
+const getRandomValue = (min, max) => {
+  return Math.floor(Math.random() * (min - max) + min);
+};
+
+const app = Vue.createApp({
+  data() {
+    return {
+      monsterHealth: 100,
+      playerHealth: 100,
+    };
+  },
+  methods: {
+    attackMonster() {
+      this.monsterHealth -= getRandomValue(5, 12);
+      this.attackPlayer();
+    },
+    attackPlayer() {
+      this.monsterHealth -= getRandomValue(8, 15);
+    },
+  },
+});
+
+app.mount("#div");
